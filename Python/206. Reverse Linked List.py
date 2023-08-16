29106673f446e89ec8247eeec6369f717cfc606b
Arukuen <sarmientoalquen@gmail.com>
2023-08-16 21:24:05 +0800

    206. Reverse Linked List

---
+++ Python/206. Reverse Linked List.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev = None
+        curr = head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
+
+
+
+
+list1 = [1,2,3,4,5]
+
+def lst2link(lst):
+    cur = dummy = ListNode(0)
+    for e in lst:
+        cur.next = ListNode(e)
+        cur = cur.next
+    return dummy.next
+
+
+out = Solution().reverseList(lst2link(list1))
+output = []
+while out:
+    output.append(out.val)
+    out = out.next
+
+print(output)
+        
