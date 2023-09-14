494bad06e8b5b46bc24bb9506609960c365bfa2a
Arukuen <sarmientoalquen@gmail.com>
2023-09-14 21:39:58 +0800

    141. Linked List Cycle.py: add slow and fast algo

--- Python/141. Linked List Cycle.py
+++ Python/141. Linked List Cycle.py
@@ -16,3 +16,14 @@ class Solution:
                 hash.add(id(head))
             head = head.next
         return False
+    
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if fast == slow:
+                return True
+        return False
